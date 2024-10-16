27e50aef700c93a9342e1b20224e9d890027e6d3
Mozes <maung892@mtroyal.ca>
2024-10-16 17:20:56 -0600

    starting browse page

---
+++ pmanz282/Browse.php
@@ -0,0 +1,37 @@
+<?php
+require_once './Includes/Config.inc.php';
+require_once './Includes/db-classes.inc.php';
+require_once './Includes/db-helpers.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index-page.css">
+    <title>Browse Races</title>
+</head>
+
+<body>
+    <header>
+        <h2>F1 Dashboard Project</h2>
+        <nav>
+            <!-- Home button -->
+            <a href="index.php">
+                Home
+            </a>
+            <!-- Browse button -->
+            <a href="Browse.php">
+                Browse
+            </a>
+            <!-- APIs button -->
+            <a href="DriverPage.php"> <!--To be changed when page is made-->
+                APIs
+            </a>
+        </nav>
+    </header>
+</body>
+
+</html>
